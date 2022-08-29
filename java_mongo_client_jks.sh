f82a94ba71e1809e358f3b05941c22756484765b
Subham Mandal <subhammandal01@gmail.com>
2022-08-29 10:48:41 +0530

    Update java_mongo_client_jks.sh

--- java_mongo_client_jks.sh
+++ java_mongo_client_jks.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+function log() {
+    echo "$1" >&2
+}
+
+function die() {
+    log "$1"
+    exit 1
+}
+
+function check_exist() {
+    [ ! -z "$(command -v java)" ] || die "The 'java' command is missing - Please install"
+    [ ! -z "$(command -v openssl)" ] || die "The the 'openssl' command is missing - Please install"
+    [ ! -z "$(command -v keytool)" ] || die "The the 'keytool' command is missing - Please install"
+    [ ! -z "$(command -v docker)" ] || die "The the 'docker' command is missing - Please install"
+    [ ! -z "$(command -v docker-compose)" ] || die "The the 'docker-compose' command is missing - Please install"
+}
+
+
+check_exist
+
+
 yes | keytool -import -file mongoCA.crt -alias mongo-client -keystore mongo-client-truststore.jks -storepass trustme
 
 openssl pkcs12 -export -out mongo-client.p12 -in mongo-client.crt -inkey mongo-client.key -name mongo-client -passin pass:trustme -passout pass:trustme -certfile mongoCA.crt
